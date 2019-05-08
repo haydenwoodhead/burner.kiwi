27a009be59dc3192b66bc417ac57139a5e693277
Christopher C. Wells <chris@chrxs.net>
2019-05-08 07:47:40 -0700

    Add SQLite3 support.

---
+++ data/sqlite3/sqlite3_test.go
@@ -0,0 +1,60 @@
+package sqlite3
+
+import (
+	"database/sql"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/google/uuid"
+
+	"github.com/haydenwoodhead/burner.kiwi/data"
+)
+
+func TestSQLite3(t *testing.T) {
+	db := GetSQLite3DB("test.sqlite3")
+	db.CreateTables()
+
+	// iterate over the testing suite and call the function
+	for _, f := range data.TestingFuncs {
+		f(t, db)
+	}
+
+	testTTLDelete(t, db)
+
+	// remove test database
+	err := os.Remove("test.sqlite3")
+	if err != nil {
+		t.Fatalf("SQLite3: failed to delete test database file")
+	}
+}
+
+func testTTLDelete(t *testing.T, db *SQLite3) {
+	db.MustExec("DELETE FROM inbox")
+	db.MustExec("DELETE FROM message")
+
+	i1 := data.Inbox{
+		ID:      uuid.Must(uuid.NewRandom()).String(),
+		Address: "hayden@example.com",
+		TTL:     time.Now().Add(-1 * time.Hour).Unix(),
+	}
+	err := db.SaveNewInbox(i1)
+	assert.NoError(t, err)
+
+	i2 := data.Inbox{
+		ID:      uuid.Must(uuid.NewRandom()).String(),
+		Address: "bobby@example.com",
+		TTL:     time.Now().Add(1 * time.Hour).Unix(),
+	}
+	err = db.SaveNewInbox(i2)
+	assert.NoError(t, err)
+
+	count, err := db.runTTLDelete()
+	assert.NoError(t, err)
+	assert.Equal(t, 1, count)
+
+	_, err = db.GetInboxByID(i1.ID)
+	assert.Equal(t, sql.ErrNoRows, err)
+}
