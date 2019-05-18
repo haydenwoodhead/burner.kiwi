17be0edfd4f057e1d42fad750aa2153a037181e6
Hayden Woodhead <hayden.sw@icloud.com>
2019-05-18 16:22:29 +1200

    Add ory/dockertest for DynamoDB tests

--- data/dynamodb/dynamodb_test.go
+++ data/dynamodb/dynamodb_test.go
@@ -1,18 +1,65 @@
 package dynamodb
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/haydenwoodhead/burner.kiwi/data"
+	"github.com/ory/dockertest"
 )
+
+var dynamoDBAddress string
+
+func TestMain(m *testing.M) {
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	resource, err := pool.Run("amazon/dynamodb-local", "1.11.477", []string{})
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+
+	dynamoDBAddress = fmt.Sprintf("http://localhost:%s", resource.GetPort("8000/tcp"))
+
+	if err := pool.Retry(func() error {
+		client := http.Client{
+			Timeout: 10 * time.Second,
+		}
+		resp, err := client.Get(dynamoDBAddress)
+		if err == nil {
+			resp.Body.Close()
+			return nil
+		}
+		return err
+	}); err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	code := m.Run()
+
+	if err := pool.Purge(resource); err != nil {
+		log.Fatalf("Could not purge resource: %s", err)
+	}
+
+	os.Exit(code)
+}
 
 func TestDynamoDB(t *testing.T) {
 	sess, err := session.NewSession(&aws.Config{
-		Region:   aws.String("us-west-2"),
-		Endpoint: aws.String("http://localhost:8000")})
+		Credentials: credentials.NewStaticCredentials("id", "secret", "token"),
+		Region:      aws.String("us-west-2"),
+		Endpoint:    aws.String(dynamoDBAddress)},
+	)
 
 	if err != nil {
 		t.Fatalf("DynamoDB: failed to setup db: %v", err)
@@ -21,7 +68,7 @@ func TestDynamoDB(t *testing.T) {
 	dbSvc := dynamodb.New(sess)
 
 	db := &DynamoDB{
-		dynDB: dbSvc,
+		dynDB:                 dbSvc,
 		emailAddressIndexName: "email_address-index",
 		emailsTableName:       "emails",
 	}
