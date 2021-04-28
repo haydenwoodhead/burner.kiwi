76154af3f07a1d462119f0c641d5a3a032addf5c
Hayden Woodhead <hayden.sw@icloud.com>
2021-04-28 20:28:10 +1200

    Bump go to 1.16 and remove golangci-lint from mod

--- tools.go
+++ tools.go
@@ -4,7 +4,6 @@ package main
 
 import (
 	_ "github.com/gobuffalo/packr/packr"
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/mattn/goveralls"
 	_ "github.com/tdewolff/minify/cmd/minify"
 	_ "golang.org/x/tools/cmd/cover"
